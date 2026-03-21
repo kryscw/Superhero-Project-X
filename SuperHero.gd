44fa40a9d26a0c3107fa071f82ee9bb7ef691c57
TheOpKingsman <siddiqaffan55@gmail.com>
2026-03-21 12:40:00 -0500

    Added the array

--- SuperHero.gd
+++ SuperHero.gd
@@ -1,38 +1,40 @@
 extends Node2D
 
+var heroes = []
+
 func _ready():
+	load_heroes_from_csv()
+	print("Heroes loaded:", heroes.size())
+
+	# TEST (remove later if needed)
+	print(heroes)
+
+func load_heroes_from_csv():
 	var file = FileAccess.open("res://HackBeta - Superhero CSV.csv", FileAccess.READ)
 
 	if file == null:
 		print("File not found!")
 		return
 
-	var heroes = []
-
 	# skip header
-	var header = file.get_line()
+	file.get_line()
 
 	while not file.eof_reached():
 		var line = file.get_line().strip_edges()
 
-		# skip empty lines
 		if line == "":
 			continue
 
 		var data = line.split(",")
 
-		# safety check (VERY IMPORTANT)
 		if data.size() < 4:
 			continue
 
 		var hero = {
 			"name": data[0].strip_edges(),
-			"power": int(data[1].strip_edges()),
-			"strength": int(data[2].strip_edges()),
-			"magic": int(data[3].strip_edges())
+			"power": int(data[1]),
+			"strength": int(data[2]),
+			"magic": int(data[3])
 		}
 
 		heroes.append(hero)
-
-	print("Total heroes loaded:", heroes.size())
-	print(heroes)
