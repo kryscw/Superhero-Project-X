f5016acaf340a28414882bcb155c1fb405692efe
TheOpKingsman <siddiqaffan55@gmail.com>
2026-03-21 16:09:55 -0500

    Seperated Heroes and Villains?

--- SuperHero.gd
+++ SuperHero.gd
@@ -4,41 +4,46 @@ var heroes = []
 var villains = []
 
 func _ready():
-	load_heroes_from_csv()
-	await get_tree().create_timer(1).timeout
-	print("HERO: " + str(heroes[0]))
-	print("VILLAIN: " + str(villains[0]))
-
-func load_heroes_from_csv():
 	var file = FileAccess.open("res://HackBeta - Superhero CSV.csv", FileAccess.READ)
 
 	if file == null:
 		print("File not found!")
 		return
 
-	# skip header
-	file.get_line()
+	file.get_line() # skip header
 
 	while not file.eof_reached():
-		var line = file.get_line().strip_edges()
+		var line = file.get_line()
+
+		if line == null:
+			continue
+
+		line = line.strip_edges()
 
 		if line == "":
 			continue
 
 		var data = line.split(",")
 
-		if data.size() < 4:
+		if data.size() < 2:
 			continue
 
-		var hero = {
+		# 👇 GET LAST COLUMN (THIS FIXES EVERYTHING)
+		var is_villain_raw = data[data.size() - 1].strip_edges().to_lower()
+
+		var is_villain = (is_villain_raw == "true")
+
+		var character = {
 			"name": data[0].strip_edges(),
 			"power": int(data[1]),
 			"strength": int(data[2]),
-			"magic": int(data[3]),
-			"isvillain": data[18]
+			"magic": int(data[3])
 		}
-	
-		if hero["isvillain"] == " False":
-			heroes.append(hero)
+
+		if is_villain:
+			villains.append(character)
 		else:
-			villains.append(hero)
+			heroes.append(character)
+
+	print("Heroes:", heroes.size())
+	print("Villains:", villains.size())
